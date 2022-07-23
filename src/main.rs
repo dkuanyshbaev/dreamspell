7c667db956fc719e6087e57056c32ace1c05dfcc
Denis K <konsistentsi@gmail.com>
2022-07-23 16:46:52 +1000

    ADD: cors support

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,37 @@
 // Dreamspell server
 // ---------------------------------------
 use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Header,
     response::{content, Redirect},
-    State,
+    Request, Response,
 };
 use rocket_dyn_templates::Template;
 
 #[macro_use]
 extern crate rocket;
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/")]
 fn home() -> Template {
@@ -19,6 +43,9 @@ fn home() -> Template {
 async fn calc(date: String) -> content::RawJson<String> {
     content::RawJson(date)
 }
+
+#[options("/<_..>")]
+fn options() {}
 
 #[catch(404)]
 pub fn not_found() -> Redirect {
@@ -33,7 +60,8 @@ pub fn internal_error() -> Redirect {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![home, calc])
+        .mount("/", routes![home, calc, options])
         .register("/", catchers![not_found, internal_error])
         .attach(Template::fairing())
+        .attach(Cors)
 }
