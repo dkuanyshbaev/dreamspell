3fc031882caeb8193cd66d45b8612b4c75097072
Denis K <konsistentsi@gmail.com>
2022-07-23 20:48:26 +1000

    ADD: calc

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ use rocket::{
     http::{Header, Status},
     outcome::Outcome,
     request::{self, FromRequest},
-    response::{content, Redirect},
+    response::Redirect,
+    serde::{json::Json, Serialize},
     Request, Response,
 };
 use rocket_dyn_templates::Template;
+
+// type DreamspellResult<T> = Result<T, DreamspellError>;
 
 const APIKEY: &str = "tzolkin";
 
@@ -40,6 +43,7 @@ impl Fairing for Cors {
 #[derive(Debug)]
 pub enum DreamspellError {
     Unauthorized,
+    Other,
 }
 
 #[derive(Debug)]
@@ -64,14 +68,40 @@ impl<'r> FromRequest<'r> for ApiKey {
     }
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Result {
+    kin: u32,
+}
+
+// impl<'r> Responder<'r, 'r> for DreamspellError {
+//     fn respond_to(self, _: &Request) -> rocket::response::Result<'r> {
+//         match self {
+//             DreamspellError::Unauthorized => Err(Status::Unauthorized),
+//             _ => Err(Status::InternalServerError),
+//         }
+//     }
+// }
+
 #[get("/")]
 fn home() -> Template {
     Template::render("home", rocket_dyn_templates::context! {})
 }
 
 #[post("/calc", format = "application/json", data = "<date>")]
-async fn calc(_key: ApiKey, date: String) -> content::RawJson<String> {
-    content::RawJson(date)
+async fn calc(date: String) -> Json<Result> {
+    // async fn calc(_key: ApiKey, date: String) -> content::RawJson<String> {
+
+    let date_parts: [u32; 3] = date
+        .split(".")
+        .map(|s| s.parse::<u32>().unwrap_or(0))
+        .collect::<Vec<u32>>()
+        .try_into()
+        .unwrap_or([0; 3]);
+
+    Json(Result {
+        kin: kin(&date_parts),
+    })
 }
 
 #[options("/<_..>")]
@@ -94,4 +124,27 @@ fn rocket() -> _ {
         .register("/", catchers![not_found, internal_error])
         .attach(Template::fairing())
         .attach(Cors)
+}
+
+const MONTH_TABLE: [u32; 12] = [0, 31, 59, 90, 120, 151, 181, 212, 243, 13, 44, 74];
+const YEAR_TABLE: [u32; 52] = [
+    232, 77, 182, 27, 132, 237, 82, 187, 32, 137, 242, 87, 192, 37, 142, 247, 92, 197, 42, 147,
+    252, 97, 202, 47, 152, 257, 102, 207, 52, 157, 2, 107, 212, 57, 162, 7, 112, 217, 62, 167, 12,
+    117, 222, 67, 172, 17, 122, 227, 72, 177, 22, 127,
+];
+
+fn kin(parts: &[u32; 3]) -> u32 {
+    let (day, month, year) = (parts[0], parts[1], parts[2]);
+
+    if day == 0 || month == 0 || year == 0 {
+        return 0;
+    }
+
+    let year_index = year as f32 - ((year as f32 / 52_f32).floor() * 52_f32);
+    let mut kin = day + MONTH_TABLE[month as usize - 1] + YEAR_TABLE[year_index as usize];
+
+    if kin > 260 {
+        kin = kin - 260
+    }
+    kin
 }
